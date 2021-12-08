58aa6d7432dc636c9033efbc288a8cb21dcacaf0
Jia Chao <jiachao2130@126.com>
2021-12-08 22:50:46 +0800

    z transform

---
+++ rust/6.z-字形变换.rs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=6 lang=rust
+ *
+ * [6] Z 字形变换
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+
+    }
+}
+// @lc code=end
+
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,126 +1,70 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-use std::rc::Rc;
-
 fn main() {
     println!("Hello, world!");
-    let s = String::from("aacabdkacaa");
-    let s = String::from("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-    longest_palindrome(s);
     test();
 }
 
 fn test() {
-    let a = Rc::new(45);
-    let b = a.clone();
-    if a == b {
-        println!("a == b");
+    let s = String::from("PAYPALISHIRING");
+    let n = 3;
+    let mut res = String::new();
+    if n == 1 {
+        return ()
+    } else {
+        let sb = s.as_bytes();
+        if sb.len() < n + 1 {
+            return ()
+        }
+
+        // init lines
+        let mut sv = Vec::new();
+        for i in 0..n {
+            sv.push(vec![]);
+        }
+
+        // 循环
+        let mut t = Vec::new();
+        let mut i = 0;
+        while i < n {
+            t.push(i);
+            i += 1;
+        }
+        println!("{:?}", t);
+
+        i = n - 2;
+        while i > 0 {
+            t.push(i);
+            i -= 1;
+        }
+        // 循环
+        println!("{:?}", t);
+
+        // 开始
+        let c = sb.len() / t.len();
+
+        // 无须判断的循环
+        let mut index = 0;
+        for i in 1..c + 1 {
+            for j in &t {
+                sv[*j].push(sb[index]);
+                index += 1;
+                println!("{}", index);
+            }
+        }
+
+        for i in 0..sb.len() - c * t.len() {
+            sv[t[i]].push(sb[c * t.len() + i]);
+        }
+
+        for i in 0..n {
+            res.push_str(&String::from_utf8(sv[i].to_vec()).unwrap());
+        }
     }
+
+    println!("{}", "PAYPALISHIRING");
+    println!("{}", res);
+    println!("{}", "PAHNAPLSIIGYIR");
 }
 
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
-}
-
-
-pub fn longest_palindrome(s: String) -> String {
-    let _buffer = s.as_bytes();
-    if _buffer.len() == 1 {
-        return s
-    }
-
-    let mut buffer = Vec::new();
-    let mut store: HashMap<u8, Vec<usize>> = HashMap::new();
-    let mut pstore: HashMap<u8, Rc<u8>> = HashMap::new();
-    for i in 0.._buffer.len() {
-        if let Some(arr) = store.get_mut(&_buffer[i]) {
-            arr.push(i);
-            buffer.push(pstore[&_buffer[i]].clone());
-        } else {
-            pstore.insert(_buffer[i], Rc::new(_buffer[i]));
-            store.insert(_buffer[i], vec![i]);
-            buffer.push(pstore[&_buffer[i]].clone());
-        }
-    }
-
-    let mut singles: Vec<usize> = Vec::new();
-    for e in store.iter().filter(|x| x.1.len() == 1) {
-       singles.push(e.1[0]);
-    }
-    singles.sort();
-    println!("{:?}", singles);
-
-    let mut res = String::from_utf8(vec![_buffer[0]]).unwrap();
-    let mut head = 0;
-    let mut tail;
-    let mut c;
-    let mut indexs;
-    let mut i;
-    let mut len;
-    let mut max = 1;
-    let mut next;
-    // 从左开始
-    while head < buffer.len() {
-        // 得到当前位置的元素
-        c = *buffer[head];
-        indexs = store.get(&c).unwrap();
-        // 长度为 1 的略过
-        if indexs.len() == 1 {
-            head += 1;
-            continue;
-        }
-        i = indexs.len() - 1;
-
-        println!("head: {}", head);
-        while head < indexs[i] {
-            tail = indexs[i];
-            len = tail - head + 1;
-            // 后面都不用试了，下一个元素 i += 1
-            if len <= max {
-                break
-            }
-            next = false;
-            // 单个元素位置检查
-            for i in singles.iter() {
-                println!("{}", i);
-                if head < *i && *i < tail {
-                    if *i != (head + tail) / 2 {
-                        next = true;
-                        break;
-                    }
-                }
-            }
-
-            if next {
-                i -= 1;
-                continue;
-            }
-            //
-            let mut l = head;
-            let mut r = tail;
-            // 这里开始匹配
-            loop {
-                l += 1;
-                r -= 1;
-                if l > r {
-                    if max < len {
-                        max = len;
-                        println!("max: {}", max);
-                        res = String::from_utf8(_buffer[head..tail + 1].to_vec()).unwrap()
-                    }
-                    break
-                }
-                if buffer[l] != buffer[r] {
-                    break
-                }
-            }
-            // 匹配失败，tail 左移再来
-            i -= 1;
-        }
-
-        //println!("head: {}", head);
-        head += 1;
-    }
-
-    res
 }
