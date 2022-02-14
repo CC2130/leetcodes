57cf045de62b418e7cfa208981732fac36507415
Jia Chao <jiachao2130@126.com>
2022-02-14 11:19:29 +0800

    接雨水

--- rust/42.接雨水.rs
+++ rust/42.接雨水.rs
@@ -7,7 +7,31 @@
 // @lc code=start
 impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
+        let mut start = &0;
+        let mut pool = vec![];
+        let mut res = 0;
 
+        for i in &height {
+            if i > start {
+                for n in pool {
+                    res += start - n;
+                }
+                pool = vec![];
+                start = i;
+            } else {
+                pool.push(*i);
+            }
+        }
+
+        if pool.len() > 1 {
+            pool.reverse();
+            // 可能出现两头最大，死循环，不妨加高一下
+            pool.push(*start + 1);
+            res += Solution::trap(pool);
+            res
+        } else {
+            res
+        }
     }
 }
 // @lc code=end
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,15 +1,43 @@
 fn main() {
     println!("Hello, world!");
     println!("res: {}", is_happy(2));
+    let h = vec![0,1,0,2,1,0,1,3,2,1,2,1];
+    println!("{}", trap(h));
 }
 
 
 /// 接雨水
 /// 从第一个非零数为基准，位置为 a
-/// 向右找到小于基准数的位置，例 x， 则池范围为 a 至 x -1
+/// 向右找到大于基准数的位置，例 x， 则池范围为 a 至 x -1
+/// 水平面高度为范围内第二高的数字
 /// 基准为 x -1，重复上步
 fn trap(height: Vec<i32>) -> i32 {
-    0
+    let mut start = &0;
+    let mut pool = vec![];
+    let mut res = 0;
+    for n in &height {
+        if n > start {
+            for i in pool {
+                res += start - i;
+                println!("i: {}", i);
+            }
+            start = n;
+            pool = vec![];
+        } else {
+            pool.push(*n);
+        }
+    }
+
+    // 处理余下的 pool
+    if pool.len() > 1 {
+        pool.reverse();
+        pool.push(*start);
+        println!("pool: {:?}", pool);
+        res += trap(pool);
+        res
+    } else {
+        res
+    }
 }
 
 fn is_happy(n: i32) -> bool {
